fbf9824a2739a438c3892fae96d055b57747045b
Jose Zamudio <zamudio@outlook.com>
2023-03-12 11:29:26 -0600

    router stuff

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,38 @@
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { PreloadAllModules, provideRouter, RouteReuseStrategy, RouterModule, withPreloading } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+  withPreloading,
+  withRouterConfig,
+} from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { NgxNotificationMsgModule } from 'ngx-notification-msg';
 import { NgxPopperjsModule } from 'ngx-popperjs';
 import { APP_ROUTES } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
-import localeEs from '@angular/common/locales/es';
-import { registerLocaleData } from '@angular/common';
 registerLocaleData(localeEs);
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
-
+    provideRouter(
+      APP_ROUTES,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'disabled',
+      }),
+      withEnabledBlockingInitialNavigation(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+        onSameUrlNavigation: 'reload',
+      }),
+      withPreloading(PreloadAllModules)
+    ),
     importProvidersFrom([
       NgxPopperjsModule,
       FontAwesomeModule,
@@ -24,7 +41,7 @@ bootstrapApplication(AppComponent, {
       NgxNotificationMsgModule,
     ]),
     provideAnimations(),
-    { provide: LOCALE_ID, useValue: 'es'}
+    { provide: LOCALE_ID, useValue: 'es' },
     // { provide: RouteReuseStrategy, useClass: Router }
   ],
 }).catch((err) => console.error(err));
